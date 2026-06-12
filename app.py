68e9f8946da913301e5f2489693a0e3df27ff5dd
rozer <shubhamsinha07084@gmail.com>
2026-06-13 00:29:33 +0530

    UI - created sidebar - created input sections

--- app.py
+++ app.py
@@ -32,3 +32,66 @@ st.markdown("""
 Predict whether an employee is likely to leave the organization
 using a Deep Learning (ANN) model trained on IBM HR Analytics data.
 """)
+
+# Sidebar for input features
+
+st.sidebar.header("Model Information")
+
+st.sidebar.info("""
+Dataset: IBM HR Analytics
+
+Model: Artificial Neural Network
+
+Architecture:
+44 → 64 → 32 → 16 → 1
+
+AUC Score: 0.785
+
+Class Weighting: Yes
+
+Threshold: 0.6
+""")
+
+# Create Input Sections
+
+st.subheader("Personal Information")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    age = st.number_input(
+        "Age",
+        min_value=18,
+        max_value=60,
+        value=30
+    )
+
+    gender = st.selectbox(
+        "Gender",
+        ["Female", "Male"]
+    )
+
+    marital_status = st.selectbox(
+        "Marital Status",
+        ["Divorced", "Married", "Single"]
+    )
+
+with col2:
+    education = st.slider(
+        "Education",
+        1,
+        5,
+        3
+    )
+
+    education_field = st.selectbox(
+        "Education Field",
+        [
+            "Life Sciences",
+            "Medical",
+            "Marketing",
+            "Other",
+            "Technical Degree",
+            "Human Resources"
+        ]
+    )
