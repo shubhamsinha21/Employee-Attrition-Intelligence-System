10e42b1ccd1d11a78efa9db853fc796de8057940
rozer <shubhamsinha07084@gmail.com>
2026-06-13 00:44:43 +0530

    Updated app.py
    - ceeated featured columns
    - fill numerical features
    - created prediction button

--- app.py
+++ app.py
@@ -207,6 +207,13 @@ with col1:
         min_value=0,
         value=3
     )
+    
+    daily_rate = st.number_input(
+    "Daily Rate",
+    min_value=100,
+    value=800
+    )
+
 
 with col2:
 
@@ -227,6 +234,25 @@ with col2:
         min_value=0,
         value=2
     )
+    
+    hourly_rate = st.number_input(
+        "Hourly Rate",
+        min_value=30,
+        value=65
+)
+
+    monthly_rate = st.number_input(
+        "Monthly Rate",
+         min_value=1000,
+         value=15000
+)
+
+    percent_salary_hike = st.number_input(
+        "Percent Salary Hike",
+         min_value=10,
+         max_value=25,
+         value=15
+)
 
 
 # Work metrics
@@ -279,3 +305,83 @@ with col2:
         4,
         3
     )
+    
+# Create predict button
+
+feature_columns = [
+    'Age',
+    'DailyRate',
+    'DistanceFromHome',
+    'Education',
+    'EnvironmentSatisfaction',
+    'HourlyRate',
+    'JobInvolvement',
+    'JobLevel',
+    'JobSatisfaction',
+    'MonthlyIncome',
+    'MonthlyRate',
+    'NumCompaniesWorked',
+    'PercentSalaryHike',
+    'PerformanceRating',
+    'RelationshipSatisfaction',
+    'StockOptionLevel',
+    'TotalWorkingYears',
+    'TrainingTimesLastYear',
+    'WorkLifeBalance',
+    'YearsAtCompany',
+    'YearsInCurrentRole',
+    'YearsSinceLastPromotion',
+    'YearsWithCurrManager',
+    'BusinessTravel_Travel_Frequently',
+    'BusinessTravel_Travel_Rarely',
+    'Department_Research & Development',
+    'Department_Sales',
+    'EducationField_Life Sciences',
+    'EducationField_Marketing',
+    'EducationField_Medical',
+    'EducationField_Other',
+    'EducationField_Technical Degree',
+    'Gender_Male',
+    'JobRole_Human Resources',
+    'JobRole_Laboratory Technician',
+    'JobRole_Manager',
+    'JobRole_Manufacturing Director',
+    'JobRole_Research Director',
+    'JobRole_Research Scientist',
+    'JobRole_Sales Executive',
+    'JobRole_Sales Representative',
+    'MaritalStatus_Married',
+    'MaritalStatus_Single',
+    'OverTime_Yes'
+]
+
+# Create predict button
+
+if st.button("Predict Attrition Risk"):
+        input_data = pd.DataFrame(
+        np.zeros((1, len(feature_columns))),
+        columns=feature_columns
+    )
+        
+# Fill numeric features
+
+        input_data['Age'] = age
+        input_data['DistanceFromHome'] = distance_from_home
+        input_data['Education'] = education
+        input_data['EnvironmentSatisfaction'] = environment_satisfaction
+        input_data['JobInvolvement'] = job_involvement
+        input_data['JobLevel'] = job_level
+        input_data['JobSatisfaction'] = job_satisfaction
+        input_data['MonthlyIncome'] = monthly_income
+        input_data['NumCompaniesWorked'] = num_companies_worked
+        input_data['PerformanceRating'] = performance_rating
+        input_data['RelationshipSatisfaction'] = relationship_satisfaction
+        input_data['StockOptionLevel'] = stock_option_level
+        input_data['TotalWorkingYears'] = total_working_years
+        input_data['TrainingTimesLastYear'] = training_times_last_year
+        input_data['WorkLifeBalance'] = work_life_balance
+        input_data['YearsAtCompany'] = years_at_company
+        input_data['YearsInCurrentRole'] = years_in_current_role
+        input_data['YearsSinceLastPromotion'] = years_since_last_promotion
+        input_data['YearsWithCurrManager'] = years_with_curr_manager
+
